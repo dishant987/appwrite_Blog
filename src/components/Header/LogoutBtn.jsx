276d6c33d540a90a78f525ef5d46e4c8d43cf5d2
dishant987 <tankdishant2003@gmail.com>
2024-06-10 21:19:37 +0530

    change

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const LogoutBtn = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate()
   const logoutHandler = () => {
     
     authService.logout().then(() => {
     
       dispatch(logout());
       toast.success("Logout Successfull!")
+      navigate('/login')
+
     });
-  
+ 
   };
   return (
     <button
